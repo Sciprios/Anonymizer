045f8ad36bc09120aed25e58a3ca5b714ba6a406
Andrew Barnes <andrew.barnes@students.plymouth.ac.uk>
2016-07-12 20:19:54 +0100

    Colour added.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """ Module to be executed to anonymize all XML files within the 'Data' folder. """
 
 if __name__ == '__main__':  # If this module is being executed.
-    print("==========Anonymizer==========")
-    
+    print("\033[92m==========Anonymizer==========\033[0m")
