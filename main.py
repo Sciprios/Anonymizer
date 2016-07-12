56504bc000fa4ed11973a386b52c3cb33d205b59
Andrew Barnes <andrew.barnes@students.plymouth.ac.uk>
2016-07-12 20:56:10 +0100

    Now extracts initial data folder.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 """ Module to be executed to anonymize all XML files within the 'Data' folder. """
+try:
+    from classes.folder import Folder
+except Exception as e:
+    print("Ensure you have the classes.py file.")
 
 if __name__ == '__main__':  # If this module is being executed.
     print("\033[92m==========Anonymizer==========\033[0m")
+    data_folder = Folder("C:\git\Anonymizer\Data")
+    data_folder._extract_self()
