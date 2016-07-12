17ca2fc485d32f23c68d329ba121ced9e95a706e
Andrew Barnes <andrew.barnes@students.plymouth.ac.uk>
2016-07-12 20:55:48 +0100

    Classes package added.

--- classes/folder.py
+++ classes/folder.py
@@ -6,26 +6,36 @@ except Exception as e:
     exit()
 
 
-class Folder(Location):
+class Folder(object):
     """ A folder object represents a folder potentially containing xml files. """
 
     def __init__(self, abs_path):
         """ Initializes a Folder object with the given absolute path and empty contents. """
-        Location.__init__(self, abs_path)
-
         contents = abs_path.split('/') # Get a pretty name for the folder.
         self.name = contents[len(contents) - 1] # This folder's name.
         self.xml_files = [] # Names of any xml files within this folder.
         self.folders = []   # Folders inside this folder.
         self.absolute_path = abs_path   # Path of this folder.
 
+        #Extract 
+
     def _extract_self(self):
         """ Extracts this folder's contents. """
-        print("\033[94m=== Extracting contents for folder: " + self.name + " ===\033[0m")
+        print("\033[94m=== Identifying contents for folder: " + self.name + " \033[0m")
+        
+        # Identify XML files.
         print("\033[93m=== Identifying XML files: \033[0m")
         self.xml_files = []
         for file in next(os.walk(self.absolute_path))[2]:
             if file.endswith(".xml"):
                 print("=== Found: " + file)
                 self.xml_files.append(file)
-        print("\033[93m=== Identified {} xml files.\033[0m".format(len(self.xml_files)))
+        print("\033[93m=== Identified {} xml file(s).\033[0m".format(len(self.xml_files)))
+
+        # Identify folders.
+        print("\033[93m=== Identifying folders: \033[0m")
+        self.xml_files = []
+        for folder in next(os.walk(self.absolute_path))[1]:
+            print("=== Found: " + folder)
+            self.folders.append(Folder(self.absolute_path + "{}/".format(folder)))
+        print("\033[93m=== Identified {} folder(s).\033[0m".format(len(self.folders)))
