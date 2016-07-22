3000f2680cbf7fc86538293e54568ff2980dc36d
cs203ab <andrew.barnes@landregistry.gsi.gov.uk>
2016-07-22 08:16:59 +0100

    file organisation

--- start.py
+++
@@ -1,7 +0,0 @@
-from anonymizer.controllers import ControlAnonymizer
-from anonymizer.views import MainForm
-
-frm = MainForm()
-controller = ControlAnonymizer()
-frm.add_controller(controller)
-controller.start()
